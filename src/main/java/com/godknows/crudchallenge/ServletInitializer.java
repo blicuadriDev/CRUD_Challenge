e95fe76fbdc2b0f5c7d49f5c15043f39327052d0
Bruno Licuadri <blicuadri@gmail.com>
2023-07-27 18:06:09 -0300

    CRUD Challenge project started

---
+++ src/main/java/com/godknows/crudchallenge/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.godknows.crudchallenge;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(CrudchallengeApplication.class);
+	}
+
+}
