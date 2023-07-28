30f1e9772895041ca5dddb5a41537863227c03b4
blicuadriDev <blicuadriDev@gmail.com>
2023-07-27 21:08:16 -0300

    delete endpoint set up

--- src/main/java/com/godknows/crudchallenge/services/ClientService.java
+++ src/main/java/com/godknows/crudchallenge/services/ClientService.java
@@ -50,7 +50,10 @@ public class ClientService {
 		return new ClientDTO(entity);
 	}
 	
-	
+	@Transactional
+	public void delete(Long id) {
+		clientRepo.deleteById(id);
+	}
 	
 	
 	private void copyDtoToEntity(ClientDTO dto, Client entity) {
